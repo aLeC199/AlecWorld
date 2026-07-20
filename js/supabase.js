fda29216298e5a870b0b36749415467d18972210
Alec <aadeyeyedavid@gmail.com>
2026-07-20 19:38:14 +0100

    Refactor Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -2,13 +2,11 @@ const SUPABASE_URL = "https://qebxskjwfrbalztxcxzj.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_qpDmkFQPuu84OA1eccZpww_lqbYUisM";
 
-if (!window.supabase) {
-    alert("Supabase library failed to load!");
-} else {
-    alert("Supabase library loaded successfully!");
-}
+const { createClient } = supabase;
 
-const supabase = window.supabase.createClient(
+const client = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+window.supabaseClient = client;
