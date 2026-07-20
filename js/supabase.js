797d82b6ef8ac37413725212a38f7ba33a500ee8
Alec <aadeyeyedavid@gmail.com>
2026-07-20 19:06:43 +0100

    Add Supabase client initialization

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://qebxskjwfrbalztxcxzj.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_qpDmkFQPuu84OA1eccZpww_lqbYUisM";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
